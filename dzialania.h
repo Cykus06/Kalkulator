c354fbc065fedcf3b8d829fc891bba6c22060fea
Cykus06 <59611419+Cykus06@users.noreply.github.com>
2022-03-22 22:11:11 +0100

    Biblioteki

---
+++ dzialania.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include "dzialania.h"
+
+void dopelnienie(int temp, std::string& str);
+std::string dodawanie(std::string a, std::string b);
+void mnowl(const int N, std::string a, std::string b, std::vector<std::string>& dodatki);
+std::string mnozenie(std::string a, std::string b);
+bool mniejsze(std::string a, std::string b);
+std::string dzielenie(std::string a, std::string b);
+std::string silnia(std::string a);
